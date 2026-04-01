c6c173ed9e1dd62a056d1dcf40c2ec830d1cc237
Aidar Akhmetzhanov <63636145+napakenjack@users.noreply.github.com>
2026-04-01 18:52:22 +0500

    Update ProductDetails.tsx

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -92,7 +92,7 @@ export function ProductDetails() {
           </div>
           
           {/* Main Image */}
-          <div className="relative aspect-[1/3] w-full bg-neutral-100">
+          <div className="relative aspect-[2/3] w-full bg-neutral-100">
             <img
               src={product.images[activeImageIndex]}
               alt={product.name}
